a91a2e7db4d07cd854f09b22c82255863a9d75bd
luongnguyen <luongnm93@live.com>
2026-06-24 11:51:02 +0700

    Bootstrap the application

---
+++ rest-number/src/test/java/org/luongnm93/quarkus/microservices/number/NumberResourceIT.java
@@ -0,0 +1,8 @@
+package org.luongnm93.quarkus.microservices.number;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class NumberResourceIT extends NumberResourceTest {
+    // Execute the same tests but in packaged mode.
+}
